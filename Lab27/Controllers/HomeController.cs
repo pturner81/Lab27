f974b92c2b6a59bb52e22e99c181c404f1ddfeaa
Turner <turnerp81@gmail.com>
2018-08-29 16:42:56 -0400

    Initial Commit

--- Lab27/Controllers/HomeController.cs
+++ Lab27/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,7 +12,23 @@ namespace Lab27.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        { 
+            HttpWebRequest APIRequest = WebRequest.CreateHttp("https://forecast.weather.gov/MapClick.php?lat=38.427341&lon=-86.9624086&FcstType=json");
+            APIRequest.UserAgent = "Mozilla / 5.0(Windows NT 6.1; Win64; x64; rv: 47.0) Gecko / 20100101 Firefox / 47.0";
+
+            HttpWebResponse APIWebResponse = (HttpWebResponse)APIRequest.GetResponse();
+            if (APIWebResponse.StatusCode == HttpStatusCode.OK)
+            {//if we got status == 200 (errthing is good)
+                //get the data and parse it
+                StreamReader ResponseData = new StreamReader(APIWebResponse.GetResponseStream());
+
+                string Weather = ResponseData.ReadToEnd(); //reads the data from the response
+
+                //Parses the JSON data
+                JObject jsonWeather = JObject.Parse(Weather);
+                ViewBag.Weather = jsonWeather;
+
+            }
             return View();
         }
 
